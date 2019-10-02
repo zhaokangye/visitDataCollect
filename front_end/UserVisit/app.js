d7b036d7f8285ef19e8d28a0c14b728bf73a6189
ChenYHeng <503296866@qq.com>
2019-10-02 19:16:43 +0800

    授权登录修改完毕

--- front_end/UserVisit/app.js
+++ front_end/UserVisit/app.js
@@ -10,78 +10,25 @@ App({
 
   onLaunch: function () {
     console.log("onLaunch")
-    var that = this
-    this.wxLogin().then(res => {
-      //读取本地缓存
-      console.log("用户id为" + wx.getStorageSync('userid'))
-      console.log("session_key为" + wx.getStorageSync('session_key'))
-      console.log('roles:' + wx.getStorageSync('roles'))
-      this.globalData.roles = wx.getStorageSync('roles')
-    });
+    var that = this;
+    // 获取手机的机型等信息
     wx.getSystemInfo({
       success: function (res) {
         that.globalData.systemInfo = {
           windowWidth: res.windowWidth,
           windowHeigth: res.windowHeight
         };
+        console.log('getsysteminfor', res)
       },
     });
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              // 获取用户权限
-              this.globalData.roles = wx.getStorageSync('roles')
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
   },
 
-  wxLogin: function () {
-    var that = this
-    return new Promise((resolve, reject) => {
-      // 登录
-      wx.login({
-        success: res => {
-          if (res.code) {
-            // 发送 res.code 到后台换取 openId, sessionKey, unionId
-            wx.request({
-              // url: 'http://localhost:8080/login',
-              url: 'http://47.101.143.247:8080/visit-0.0.1-SNAPSHOT/login',
-              data: {
-                code: res.code
-              },
-              success: function (res) {
-                if (res.statusCode == 200) {
-                  //存入本地缓存
-                  wx.setStorageSync('token', res.data.token)
-                  wx.setStorageSync('roles', res.data.roles)
-                } else {
-                  console.log(res.errMsg)
-                }
-                resolve(res);
-              }
-            })
-          } else {
-            console.log('登录失败！' + res.errMsg)
-            reject('error');
-          }
-        }
-      })
-    })
-  },
+  
 
-
+  /**
+   * 得到该用户的角色
+   */
+  getRoles: function (nickName, code) {
+    return wx.getStorageSync('roles')
+  }
 })
--- front_end/UserVisit/pages/usertype/usertype.js
+++ front_end/UserVisit/pages/usertype/usertype.js
@@ -79,13 +79,87 @@ Page({
 
   getUserInfo: function (e) {
     console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true,
-      roles: app.globalData.roles,
-    })
+    var that = this
     
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        console.log('getSetting', res);
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              console.log('getuserinfo', res)
+              // 可以将 res 发送给后台解码出 unionId
+              app.globalData.userInfo = res.userInfo
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (app.userInfoReadyCallback) {
+                app.userInfoReadyCallback(res)
+              }
+              // 登录 得到登录的code
+              var nickname = res.userInfo.nickName;
+              that.wxLogin(nickname).then(res => {
+                //读取本地缓存
+                console.log("用户id为" + wx.getStorageSync('userid'))
+                console.log("session_key为" + wx.getStorageSync('session_key'))
+                console.log('roles:' + wx.getStorageSync('roles'))
+                app.globalData.roles = wx.getStorageSync('roles')
+
+                // 设置页面的头像以及名字
+                app.globalData.userInfo = e.detail.userInfo
+                that.setData({
+                  userInfo: e.detail.userInfo,
+                  hasUserInfo: true,
+                  roles: app.globalData.roles,
+                })
+              });
+            }
+          })
+        }
+      }
+    })
+  },
+
+  /**
+  * 登录
+  */
+  wxLogin: function (nickName) {
+    var that = this
+    return new Promise((resolve, reject) => {
+      // 登录
+      wx.login({
+        success: res => {
+          console.log(res);
+          if (res.code) {
+            var code = res.code;
+            // 发送 res.code 到后台换取 openId, sessionKey, unionId
+            wx.request({
+              // url: 'http://localhost:8080/login',
+              url: 'http://47.101.143.247:8080/visit-0.0.1-SNAPSHOT/login',
+              data: {
+                nickName: nickName,
+                code: code,
+              },
+              success: function (res) {
+                if (res.statusCode == 200) {
+                  //存入本地缓存
+                  wx.setStorageSync('token', res.data.token)
+                  wx.setStorageSync('roles', res.data.roles)
+                } else {
+                  console.log(res.errMsg)
+                }
+                console.log('wx.login.request', res)
+                resolve(res);
+              }
+            })
+          } else {
+            console.log('登录失败！' + res.errMsg)
+            reject('error');
+          }
+        }
+      })
+    })
   },
 
   /**
